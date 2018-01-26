7083955ae60c9209ac68520a7be46dc7fadeabfe
koray.ince <korayince327@hotmail.com>
2018-01-26 18:04:42 +0300

    update

--- app/src/main/java/com/sourcey/materiallogindemo/FirmaDKabulActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/FirmaDKabulActivity.java
@@ -1,13 +1,53 @@
 package com.sourcey.materiallogindemo;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class FirmaDKabulActivity extends AppCompatActivity {
+
+    private static final String TAG = "FirmaDKabulActivity";
+
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firma_dkabul);
-    }
-}
+
+        mDisplayDate = (TextView) findViewById(R.id.tarih);
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        FirmaDKabulActivity.this, android.R.style.Theme_Holo_Dialog_MinWidth, mDateSetListener, year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + dayOfMonth + "/" + year);
+
+                String date = month + "/" + dayOfMonth + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
+    }};
